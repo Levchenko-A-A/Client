da38310a5edeec48445410e2716b921b7fa3bef7
Андрей Левченков <lev_a_a@mail.ru>
2025-03-05 21:22:09 +0200

    cl

---
+++ Client/Model/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+namespace Client.Model
+{
+    public class Client
+    {
+        [JsonIgnore]
+        public int Clientid { get; set; }
+        [JsonPropertyName("firstname")]
+        public string Firstname { get; set; } = null!;
+        [JsonPropertyName("surname")]
+        public string? Surname { get; set; }
+        [JsonPropertyName("lastname")]
+        public string Lastname { get; set; } = null!;
+        [JsonPropertyName("compane")]
+        public string Company { get; set; } = null!;
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; } = null!;
+        [JsonPropertyName("city")]
+        public string City { get; set; } = null!;
+    }
+}
